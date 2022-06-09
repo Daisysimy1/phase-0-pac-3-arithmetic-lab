8b63d18f32e4088d42683d32ad8d030c75674ca6
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-09 14:51:42 +0300

    commit tree cedf73ba6e796b5ca425ff77f38e5c5e8399a9f6

---
+++ index.js
@@ -0,0 +1,58 @@
+function add(){
+    // we'll fill this in shortly
+
+}
+
+function subtract(){
+    // we'll fill this in shortly
+
+}
+
+function multiply(){
+    // we'll fill this in shortly
+
+}
+
+function divide(){
+    // we'll fill this in shortly
+
+}
+
+function add(a, b){
+    return a+b
+}
+console.log(add(9,1))
+
+function subtract(a, b){
+    return a-b
+}
+console.log(subtract(9,4))
+
+function multiply(a, b){
+    return a*b
+}
+console.log(multiply(6,2))
+
+function divide(a, b){
+    return a/b
+}
+console.log(divide(7,3))
+
+function increment(n){
+    return ++n
+}
+console.log(increment(13))
+
+function decrement(n){
+    return --n
+}
+console.log(decrement(42))
+
+function makeInt(n){
+   return parseInt(n,10) 
+}
+console.log(makeInt("random"))
+
+function preserveDecimal(n){
+    return parseFloat(n,4.5) 
+ }
